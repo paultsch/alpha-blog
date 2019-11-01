2cadebec0c7666e0ade4116f389cf03f453f2f61
Paul Tschudin <paul.tschudin@gmail.com>
2019-11-01 14:11:26 -0500

    Continue on with database work

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 root to: 'pages#home'
 get 'about', to:'pages#about'
 
+resources :articles
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
