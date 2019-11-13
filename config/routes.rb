639ea686e1959106d290b66ac33947acd511bdce
Paul Tschudin <paul.tschudin@gmail.com>
2019-11-12 22:10:29 -0600

    Sign up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ get 'about', to:'pages#about'
 
 resources :articles
 
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
