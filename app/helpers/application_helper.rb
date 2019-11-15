8fc716317324d192acb337a3a34a848b80dec18a
Paul Tschudin <paul.tschudin@gmail.com>
2019-11-15 16:39:16 -0600

    add users listing and update show article view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "img img-circle")
   end
+  def gravatar_img_card(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.username, class: "card-img-top")
+  end
 end
