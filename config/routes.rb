f8ae64ce243290c4d571005d4bb26b0e9b0d8fa8
Paul Tschudin <paul.tschudin@gmail.com>
2019-11-22 19:41:50 -0600

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ resources :articles
 
 get 'signup', to: 'users#new'
 resources :users, except: [:new]
+
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destory'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
